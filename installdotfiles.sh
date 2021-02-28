c28b0a6d55148c1c8681b4eeee106a5b6ba50c31
wizzdom <dm.connor2003@gmail.com>
2021-02-28 17:33:13 +0000

    Fully consolidated dotfiles, more to come

--- installdotfiles.sh
+++ installdotfiles.sh
@@ -1,30 +1,34 @@
 #!/bin/sh
+echo "Install dotfiles?"
+echo "(Press ENTER to continue...)"
+read
 
-cd ~/
+cd $HOME/
 echo 'Cloning Repo' 
-git clone --recursive https://github.com/wizzdom/dotfiles ~/.dotfiles
+git clone --recursive https://github.com/wizzdom/dotfiles $HOME/.dotfiles
 ### Symlinks
 echo 'Making Symbolic Links'
-# ZSH links
-ln -sf ~/.dotfiles/zsh/.zshrc ~/.zshrc
-ln -sf ~/.dotfiles/zsh/.zshenv ~/.zshenv
-ln -sf ~/.dotfiles/zsh/.p10k.zsh ~/.p10k.zsh
+# ZSH 
+ln -sf $HOME/.dotfiles/zsh/.zprofile $HOME/.zprofile
+ln -sf $HOME/.dotfiles/zsh/.zshrc $HOME/.zshrc
+ln -sf $HOME/.dotfiles/zsh/.zshenv $HOME/.zshenv
+ln -sf $HOME/.dotfiles/zsh/.p10k.zsh $HOME/.p10k.zsh
 
-ln -sf ~/.dotfiles/.nvidia-settings-rc ~/nvidia-settings-rc
+ln -sf $HOME/.dotfiles/.nvidia-settings-rc $HOME/nvidia-settings-rc
 #  Git
-ln -sf ~/.dotfiles/.gitconfig_global ~/.gitconfig
+ln -sf $HOME/.dotfiles/.gitconfig_global $HOME/.gitconfig
 # Vim
-ln -sf ~/.dotfiles/.vimrc ~/.vimrc
-ln -sf ~/.dotfiles/.vim/ ~/.vim
-# Awesome WM (material-awesome)
-ln -sf ~/.dotfiles/.config/awesome/ ~/.config/awesome/
+ln -sf $HOME/.dotfiles/.vimrc $HOME/.vimrc
+ln -sf $HOME/.dotfiles/.vim/ $HOME/.vim
+# Awesome WM 
+ln -sf $HOME/.dotfiles/.config/awesome/ $HOME/.config/awesome/
 # application configs (.config)
-ln -sf ~/.dotfiles/.config/terminator/config ~/.config/terminator/config
-ln -sf ~/.dotfiles/.config/neofetch/config.conf ~/.config/neofetch/config.conf
-ln -sf ~/.dotfiles/.config/fontconfig ~/.config/fontconfig
-ln -sf ~/.dotfiles/.config/htop/htoprc ~/.config/htop/htoprc
-ln -sf ~/.dotfiles/.config/remmina/remmina.pref ~/.config/remmina/remmina.pref
-ln -sf ~/.dotfiles/.config/pcmanfm ~/.config/pcmanfm
-ln -sf ~/.dotfiles/.config/pnmixer/config ~/.config/pnmixer/config
+ln -sf $HOME/.dotfiles/.config/terminator/config $HOME/.config/terminator/config
+ln -sf $HOME/.dotfiles/.config/neofetch/config.conf $HOME/.config/neofetch/config.conf
+ln -sf $HOME/.dotfiles/.config/fontconfig $HOME/.config/fontconfig
+ln -sf $HOME/.dotfiles/.config/htop $HOME/.config/htop/
+# ln -sf $HOME/.dotfiles/.config/remmina/remmina.pref $HOME/.config/remmina/remmina.pref
+ln -sf $HOME/.dotfiles/.config/pcmanfm $HOME/.config/pcmanfm
+ln -sf $HOME/.dotfiles/.config/pnmixer $HOME/.config/pnmixer
 
 echo 'done.'
