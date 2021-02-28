6c900d1f1dae09321b0fe1e05050264d416d208e
wizzdom <dm.connor2003@gmail.com>
2021-02-28 18:07:11 +0000

    wOw MORE dotfiles?

---
+++ .config/screenlayout/laptop-upstairs-docked-plus.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP1 --primary --mode 1920x1080 --pos 1600x297 --rotate normal --output DP1 --mode 1024x768 --pos 3520x0 --rotate normal --output DP2 --mode 1600x900 --pos 0x0 --rotate normal --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off
