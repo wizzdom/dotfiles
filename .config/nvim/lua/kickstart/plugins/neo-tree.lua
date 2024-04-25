934d0f4c68be2e99b2b3e89ba04126c0d2c20b2b
wizzdom <dom@wizzdom.xyz>
2024-04-25 12:36:24 +0100

    nvim: add obsidian.nvim, markdown improvements, replace netrw with neo-tree

--- .config/nvim/lua/kickstart/plugins/neo-tree.lua
+++ .config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -11,6 +11,25 @@ return {
 	},
 	cmd = "Neotree",
 	keys = {},
-	opts = {},
+	init = function()
+		vim.api.nvim_create_autocmd("BufEnter", {
+			-- make a group to be able to delete it later
+			group = vim.api.nvim_create_augroup("NeoTreeInit", { clear = true }),
+			callback = function()
+				local f = vim.fn.expand("%:p")
+				if vim.fn.isdirectory(f) ~= 0 then
+					vim.cmd("Neotree current dir=" .. f)
+					-- neo-tree is loaded now, delete the init autocmd
+					vim.api.nvim_clear_autocmds({ group = "NeoTreeInit" })
+				end
+			end,
+		})
+	end,
+
+	opts = {
+		filesystem = {
+			hijack_netrw_behavior = "open_current",
+		},
+	},
 	vim.keymap.set("n", "<C-o>", "<cmd>Neotree toggle<CR>", { desc = "Toggle File Tree (Neo-tree)" }),
 }
